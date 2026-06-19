d6b7eb60a1a1aedb288b90315a93c0df98206d35
vinayyalla00 <vinayyalla12345@gmail.com>
2026-06-19 21:23:49 +0530

    This first one

---
+++ Demo.py
@@ -0,0 +1,26 @@
+
+
+
+my_list = [1, 2, 3, 4]
+
+print(my_list)
+
+# accessing
+print(my_list[0])  # first number
+
+print(my_list[-1])  # to acceses the last number 
+
+print(my_list[0:4])  
+
+#modifiy
+
+my_list[3] =  "vinay";
+print(my_list);
+
+my_list.append("Kumar")
+print(my_list);
+
+my_list.remove("Kumar")
+my_list.pop(3)
+
+print(my_list);
